cea92e77a21763a01f99182dd96ad77d5d2998e1
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2025-06-04 18:38:38 +0800

    modify: postpage background image

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,13 +1,20 @@
 /*src\pages\PostsPage.jsx*/
 import React from 'react';
 import '../styles/PostsPage.css';
+import bg from '../assets/background_NotesPage.jpg';
 
 function Posts() {
   return (
-    <div className="posts-page">
+    <div className="posts-page" style={{
+      backgroundImage: `url(${bg})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundAttachment: 'fixed'
+    }}>
       <h1>文章總覽</h1>
       <p>這裡會列出所有文章。</p>
-    </div>
+    </div >
   );
 }
-export default Posts;
+export default Posts; 
