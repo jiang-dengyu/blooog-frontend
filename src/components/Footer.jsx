3a0fb5d620dbd014b3b4619435c9d1fd9e4771be
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2025-06-04 10:03:00 +0800

    feat: add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="footer-content">
+        <p>&copy; {new Date().getFullYear()} Blooog! All rights reserved.</p>
+        <p className="footer-note">Made with 💻 by Jiang</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
